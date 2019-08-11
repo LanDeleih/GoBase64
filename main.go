9714c43b84d2e8fa180a951af6f3c6019fa2061d
Gabriel Leem <yotsyni@gmail.com>
2019-08-12 00:17:01 +0300

    add decode func

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/base64"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"os"
 )
@@ -26,11 +27,28 @@ func encodeBase64() string {
 
 }
 
-func decodeBase64() string {
+func decodeBase64() {
 
+	// if len(os.Args) < 2 {
+	// 	fmt.Println("You missed file name")
+	// }
+
+	// file, err := ioutil.ReadFile(os.Args[1])
+
+	// if err != nil {
+	// 	fmt.Println("Can't read file:", os.Args[1])
+	// 	fmt.Println(err)
+	// }
+
+	s, err := io.Reader(os.Args[1])
+	
+	decode, _ := base64.StdEncoding.DecodeString(s)
+
+	fmt.Println(string(decode))
 }
 
 func main() {
 
-	fmt.Println(encodeBase64())
+	fmt.Println(decodeBase64)
+
 }
